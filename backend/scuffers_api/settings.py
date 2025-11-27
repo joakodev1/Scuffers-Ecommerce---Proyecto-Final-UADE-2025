5a860f3a73f2d817baad00a3eb96c1bfc8bcd3d5
joakodev1 <notjoaco@gmail.com>
2025-11-27 17:01:36 -0300

    Apunto la base de datos a MySQL de Railway

--- backend/scuffers_api/settings.py
+++ backend/scuffers_api/settings.py
@@ -82,14 +82,11 @@ WSGI_APPLICATION = 'scuffers_api.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST", "127.0.0.1"),
-        "PORT": os.getenv("DB_PORT", "3306"),
-        "OPTIONS": {
-            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+        "NAME": os.getenv("MYSQLDATABASE", "railway"),
+        "USER": os.getenv("MYSQLUSER", "root"),
+        "PASSWORD": os.getenv("MYSQLPASSWORD", ""),
+        "HOST": os.getenv("MYSQLHOST", "mysql.railway.internal"),
+        "PORT": os.getenv("MYSQLPORT", "3306"),
     }
 }
 
