11746adeff2e0d8ebbf578adfacc0fdf5df73af4
joakodev1 <notjoaco@gmail.com>
2026-04-20 02:29:43 -0300

    Fix import react-router-dom

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,6 +1,6 @@
 // src/pages/Cart.jsx
 import { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-motion";
+import { Link, useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
 
 import { useAuth } from "../auth/AuthContext.jsx";
